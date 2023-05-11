90ce9184f3e1d93e818a28d58cd7a3004b947bf2
Patrick Tumu <patricktumu@gmail.com>
2023-05-10 21:44:21 -0600

    Completed logic and tests for wordsearch assignment

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,10 +1,10 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordSearch.js');
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("Word not found test", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("Horizontal word search", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +31,71 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+  });
+
+  it("Vertical Search 1", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'R', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'A', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'K', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK');
+
+    assert.isTrue(result);
+  });
+
+  it("Vertical Search 2", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'R', 'F', 'E', 'L', 'Y'],
+      ['Y', 'F', 'C', 'A', 'Q', 'U', 'A', 'E'],
+      ['H', 'M', 'J', 'N', 'E', 'V', 'R', 'R'],
+      ['W', 'H', 'C', 'K', 'Y', 'E', 'R', 'B'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'O'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'L'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'YERBOL');
+
+    assert.isTrue(result);
+  });
+
+  it("Empty array test", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'R', 'F', 'E', 'L', 'Y'],
+      ['Y', 'F', 'C', 'A', 'Q', 'U', 'A', 'E'],
+      ['H', 'M', 'J', 'N', 'E', 'V', 'R', 'R'],
+      ['W', 'H', 'C', 'K', 'Y', 'E', 'R', 'B'],
+      [],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'L'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'YERBOL');
+
+    assert.isFalse(result);
+  });
+
+  it("Empty matrix test", function() {
+    const result = wordSearch([
+      [],
+    ], 'TEST');
+
+    assert.isFalse(result);
+  });
+
+  it("Bad input test", function() {
+    const result = wordSearch(
+      7,'TEST');
+
+    assert.isFalse(result);
   });
 });
